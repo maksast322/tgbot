5a4e2beb7489da9e04185e6b646078d981398360
d1zy <maksaks499@gmail.com>
2025-12-18 01:32:33 +0300

    Добавил бота с авторизацией через Playwright

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
+
 from playwright.sync_api import sync_playwright
 from loguru import logger
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv)
+load_dotenv(find_dotenv())
 
 LOGIN = os.getenv('LOGIN')
 PASSWORD = os.getenv('PASSWORD')
@@ -17,16 +18,28 @@ with sync_playwright() as p:
     brower = p.chromium.launch(headless=False)
     page = brower.new_page()
 
-    page.goto("https://journal.top-academy.ru/ru/main/settings/page/index")
+    page.goto("https://journal.top-academy.ru/ru/auth/login/index")
     logger.info("Страница загружена")
 
-    page.wait_for_selector('input[name="username"]', timeout=10000)
+    page.wait_for_selector('input[name="username"]', timeout=30000)
     page.fill('input[name="username"]', LOGIN)
-    page.fill('input[name="username"]', PASSWORD)
+    page.fill('input[name="password"]', PASSWORD)
     logger.info("Данные для входа введены")
 
-    page.click("button[type=`submit`]")
+    page.click('button[type="submit"]')
+    logger.info("Кнопка входа нажата")
+
+    page.wait_for_timeout(5000)
     logger.success(f"Вход выполнен! Текущий url: {page.url}")
 
+    page.goto('https://journal.top-academy.ru/ru/main/homework/page/index')
+    logger.info("Зашли на страницу с домашним заданием")
+
+    page2 = brower.new_page()
+    namestudent = input("Введите имя на github: ")
+    page2.goto('https://github.com/' + namestudent)
+    logger.info('Открыли github')
+    
     input("Нажмите Enter для закрытия...")
     brower.close()
+
--- main1.py
+++ main1.py
@@ -2,10 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 from loguru import logger
 
-url = input("Введите ссылку: ")
+url = input("Введите URL: ")
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppWebKit/537.36(KHTML, like Geco) Chrome/120.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "html.parser")
+
+response = requests.get(url, headers=headers)  
+soup = BeautifulSoup(response.text, 'html.parser')
 print(soup.text)
